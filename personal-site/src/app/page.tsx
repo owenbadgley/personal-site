5aad6273d9d1ebe366d69be21c039c386473a715
owenbadgley <thm9hh@virginia.edu>
2024-07-08 18:48:59 -0400

    Fixed Material UI import

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Grid } from "@mui/material";
+import Button from "@mui/material/Button";
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <Grid>
+      <Button variant="contained">Hello World!</Button>
+    </Grid>
   );
 }
