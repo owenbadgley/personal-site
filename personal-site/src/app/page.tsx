ee924dccfea8970f41c2122cc6c65242c03a9914
owenbadgley <thm9hh@virginia.edu>
2024-07-15 14:04:33 -0400

    Rough Draft of layout

--- personal-site/src/app/page.tsx
+++ personal-site/src/app/page.tsx
@@ -1,10 +1,17 @@
-import { Grid } from "@mui/material";
-import Button from "@mui/material/Button";
+import { Grid, Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <Grid>
-      <Button variant="contained">Hello World!</Button>
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Button>Click Me</Button>
+      </Grid>
+      <Grid item xs={1} color="red">
+        <Button>Click Me</Button>
+      </Grid>
+      <Grid item xs={11} color="green">
+        <Button>Click Me</Button>
+      </Grid>
     </Grid>
   );
 }
