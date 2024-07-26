90c3b7a3cfa520b5d54adc45da5fbab8a08b3ec6
owenbadgley <143543330+owenbadgley@users.noreply.github.com>
2024-07-26 13:59:46 -0400

    changed file structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
+import theme from "../theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
